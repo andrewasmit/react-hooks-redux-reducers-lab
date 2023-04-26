590c9cf833a1464fc4f9282b5b4725b956cafc95
Andrew <andrewasmit@hotmail.com>
2023-04-26 07:07:45 -0500

    commit tree 2b6557fc942c04a8c3bb9c8aad229b7a2055fe93

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,18 @@
-export function manageFriends(state, action) {
-  // your code here
+let initialState = { friends: [] };
+
+export function manageFriends(state = initialState, action) {
+  if(action.type ==="friends/add"){
+    return { 
+      friends: [...state.friends, {
+        name: action.payload.name,
+        hometown: action.payload.hometown,
+        id: action.payload.id
+      }]
+    }
+  } else if (action.type==="friends/remove"){
+    return {
+      friends: [...state.friends].filter(friend=>friend.id !== action.payload)
+    }
+  } else
+  return state
 }
